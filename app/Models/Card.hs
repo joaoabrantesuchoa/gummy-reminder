86b2fddcb434493fe736caf770f5a14f73b20c8c
Pedro Casado <pgscasado.pessoal@gmail.com>
2022-02-23 17:09:47 -0300

    text controller enhancement

--- app/Models/Card.hs
+++ app/Models/Card.hs
@@ -1,6 +1,5 @@
 module Models.Card where
 	data Card = Card {
-		id:: Int,
 		front:: String,
 		back:: String
 	} deriving (Show, Read)
--- app/Models/Deck.hs
+++ app/Models/Deck.hs
@@ -2,14 +2,13 @@ module Models.Deck where
 	import qualified Models.Card as C
 
 	data Deck = Deck {
-		idDeck:: Int,
 		name:: String,
 		cards:: [C.Card]
 	} deriving (Show, Read)
 
-	addCard:: Deck -> C.Card -> Deck
-	addCard deck newCard = 
-		Deck (idDeck deck) (name deck) (cards deck ++ [newCard])
+	-- addCard:: Deck -> C.Card -> Deck
+	-- addCard deck newCard = 
+	-- 	Deck (deck) (name deck) (cards deck ++ [newCard])
 
 	--setName:: Deck -> String -> Deck
 	--setName deck newName = 
