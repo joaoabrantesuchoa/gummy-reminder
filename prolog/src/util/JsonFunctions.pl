59244e41b3540567a879668b6d8691c31dc13fc4
Pedro Casado <pgscasado.pessoal@gmail.com>
2022-03-29 17:05:39 -0300

    refactor to english

--- prolog/src/util/JsonFunctions.pl
+++ prolog/src/util/JsonFunctions.pl
@@ -2,28 +2,29 @@
 
 % Fato dinâmico para gerar o id dos agentes
 id(1).
-incrementa_id :- retract(id(X)), Y is X + 1, assert(id(Y)).
+idAdd :- retract(id(X)), Y is X + 1, assert(id(Y)).
 :- dynamic id/1.
 
 % Lendo arquivo JSON puro
-lerJSON(FilePath, File) :-
+readJSON(FilePath, File) :-
 	open(FilePath, read, F),
-	json_read_dict(F, File).
+	json_read_dict(F, File),
+	close(F). 
 
 % Regras para listar todos agentes
-exibirDecksAux([]).
-exibirDecksAux([H|T]) :- 
+showDecksAux([]).
+showDecksAux([H|T]) :- 
 	write("ID:"), writeln(H.id),
 	write("Name:"), writeln(H.name), 
-	write("Cards:"), writeln(H.cards), nl, exibirDecksAux(T).
+	write("Cards:"), writeln(H.cards), nl, showDecksAux(T).
 
-exibirDecks(FilePath) :-
-	lerJSON(FilePath, Decks),
-	exibirDecksAux(Decks).
+showDecks(FilePath) :-
+	readJSON(FilePath, Decks),
+	showDecksAux(Decks).
 
 % Criando representação em formato String de um agente em JSON
-deckToJSON(Nome, Cards, ID, Out) :-
-	swritef(Out, '{"name":"%w","cards":%q,"id":%w}', [Nome, Cards, ID]).
+deckToJSON(Name, Cards, ID, Out) :-
+	swritef(Out, '{"name":"%w","cards":%q,"id":%w}', [Name, Cards, ID]).
 
 % Convertendo uma lista de objetos em JSON para 
 decksToJSON([], []).
@@ -32,48 +33,46 @@ decksToJSON([H|T], [X|Out]) :-
 	decksToJSON(T, Out).
 
 % Salvar em arquivo JSON
-salvarDeck(FilePath, Nome, Cards) :- 
-	id(ID), incrementa_id,
-	lerJSON(FilePath, File),
-	decksToJSON(File, ListaDecksJSON),
-	deckToJSON(Nome, Cards, ID, DeckJSON),
-	append(ListaDecksJSON, [DeckJSON], Saida),
-	open(FilePath, write, Stream), write(Stream, Saida), close(Stream).
-
-salvarCard(FilePath, IdDeck, NewCard) :-
-	
+writeDeck(FilePath, Name, Cards) :- 
+	id(ID), idAdd,
+	readJSON(FilePath, File),
+	decksToJSON(File, DecksListJSON),
+	deckToJSON(Name, Cards, ID, DeckJSON),
+	append(DecksListJSON, [DeckJSON], OutJSON),
+	open(FilePath, write, Stream), write(Stream, OutJSON), close(Stream).
 
 % Mudando o name de um agente
-editarNomeDeckJSON([], _, _, []).
-editarNomeDeckJSON([H|T], H.id, Nome, [_{id:H.id, name:Nome, cards:H.cards}|T]).
-editarNomeDeckJSON([H|T], Id, Nome, [H|Out]) :- 
-	editarNomeDeckJSON(T, Id, Nome, Out).
+updateDeckNameJSON([], _, _, []).
+updateDeckNameJSON([H|T], H.id, Name, [_{id:H.id, name:Name, cards:H.cards}|T]).
+updateDeckNameJSON([H|T], Id, Name, [H|Out]) :- 
+	updateDeckNameJSON(T, Id, Name, Out).
 
-editarNomeDeck(FilePath, IdDeck, NovoNome) :-
-	lerJSON(FilePath, File),
-	editarNomeDeckJSON(File, IdDeck, NovoNome, SaidaParcial),
-	decksToJSON(SaidaParcial, Saida),
-	open(FilePath, write, Stream), write(Stream, Saida), close(Stream).
+updateDeckName(FilePath, DeckId, NewName) :-
+	readJSON(FilePath, File),
+	updateDeckNameJSON(File, DeckId, NewName, Out),
+	decksToJSON(Out, OutJSON),
+	open(FilePath, write, Stream), write(Stream, OutJSON), close(Stream).
 
 % Mudando o name de um agente
-addCardsDeckJSON([], _, _, []).
-addCardsDeckJSON([H|T], H.id, NovoCard, [_{id:H.id, name:H.name, cards: NovoCard}]).
-addCardsDeckJSON([H|T], Id, NovoCard, [H|Out]) :- 
-	addCardsDeckJSON(T, Id, NovoCard, Out).
+addCardJSON([], _, _, []).
+addCardJSON([H|T], H.id, NewCard, [_{id:H.id, name:H.name, cards: Cards}]) :-
+	append([NewCard], H.cards, Cards).
+addCardJSON([H|T], Id, NewCard, [H|Out]) :- 
+	addCardJSON(T, Id, NewCard, Out).
 
-addCardsDeck(FilePath, IdDeck, NovoCard) :-
-	lerJSON(FilePath, File),
-	addCardsDeckJSON(File, IdDeck, NovoCard, SaidaParcial),
-	decksToJSON(SaidaParcial, Saida),
-	open(FilePath, write, Stream), write(Stream, Saida), close(Stream).
+addCard(FilePath, DeckId, NewCard) :-
+	readJSON(FilePath, File),
+	addCardJSON(File, DeckId, NewCard, Out),
+	decksToJSON(Out, OutJSON),
+	open(FilePath, write, Stream), write(Stream, OutJSON), close(Stream).
 
 % Removendo agente
-removerDeckJSON([], _, []).
-removerDeckJSON([H|T], H.id, T).
-removerDeckJSON([H|T], Id, [H|Out]) :- removerDeckJSON(T, Id, Out).
+deleteDeckJSON([], _, []).
+deleteDeckJSON([H|T], H.id, T).
+deleteDeckJSON([H|T], Id, [H|Out]) :- deleteDeckJSON(T, Id, Out).
 
-removerDeck(FilePath, Id) :-
-	lerJSON(FilePath, File),
-	removerDeckJSON(File, Id, SaidaParcial),
-	decksToJSON(SaidaParcial, Saida),
-	open(FilePath, write, Stream), write(Stream, Saida), close(Stream).
+deleteDeck(FilePath, Id) :-
+	readJSON(FilePath, File),
+	deleteDeckJSON(File, Id, Out),
+	decksToJSON(Out, OutJSON),
+	open(FilePath, write, Stream), write(Stream, OutJSON), close(Stream).
